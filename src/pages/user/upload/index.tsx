11ff51e15fd760baa17f2c795e473ae181899cf2
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-08-06 23:46:47 +0200

    Add a base for uploading images

--- src/pages/user/upload/index.tsx
+++ src/pages/user/upload/index.tsx
@@ -1,9 +1,42 @@
+import {type ChangeEvent, useMemo, useRef, useState } from "react";
 import UserPageLayout from "~/components/UserPageLayout";
+import Image from "next/image";
 
 export default function UploadImagePage() {
+  const [profilePhoto, setProfilePhoto] = useState<Blob | undefined | string>();
+
+  const profilePictureURL = useMemo(
+    () =>
+      profilePhoto instanceof Blob
+        ? URL.createObjectURL(profilePhoto)
+        : profilePhoto,
+    [profilePhoto]
+  );
+
+  const filePickerInput = useRef<HTMLInputElement>(null);
+
+  function handleImageFile(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.files) {
+      const [imageFile] = event.target.files;
+      setProfilePhoto(imageFile);
+    }
+  }
+
   return (
     <UserPageLayout>
       <h1>upload images</h1>
+      <Image
+        src={profilePictureURL ?? "/profile-pics/Default.png"}
+        height={300}
+        width={200}
+        alt=""
+      />
+      <input
+        onChange={handleImageFile}
+        ref={filePickerInput}
+        type="file"
+        accept="image/*"
+      />
     </UserPageLayout>
   );
 }
