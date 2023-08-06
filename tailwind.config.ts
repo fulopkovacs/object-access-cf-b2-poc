34541fc3914b95acc66002813c8666a912067c94
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-08-06 19:04:15 +0200

    Replace the default Tailwind colors with Radix colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 import { type Config } from "tailwindcss";
+import { grayDark } from "@radix-ui/colors";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+  colors: { ...grayDark },
     extend: {},
   },
   plugins: [],
