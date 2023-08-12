f34eb640a162e7f7fcd9be067bfc44a161f64656
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-08-12 21:36:25 +0200

    Add a progress bar to visualize the synchronization of the changes

--- src/pages/user/images/index.tsx
+++ src/pages/user/images/index.tsx
@@ -1,9 +1,18 @@
-import { Link, Card, CardBody, Image, Switch } from "@nextui-org/react";
+import {
+  Link,
+  Card,
+  CardBody,
+  Image,
+  Switch,
+  Progress,
+} from "@nextui-org/react";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 import prettyBytes from "pretty-bytes";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import UserPageLayout from "~/components/UserPageLayout";
 import { api } from "~/utils/api";
+import { useTimer } from "react-timer-hook";
+import { AnimatePresence, motion } from "framer-motion";
 
 type ImageData = {
   id: number;
@@ -21,15 +30,37 @@ function MakeImagePublicSwitch({ imageData }: ImageDataProps) {
   const [isPublic, setIsPublic] = useState(imageData.public);
   const utils = api.useContext();
   /**
-  It takes 60 seconds max to synchronise the
-  Cloudfare KV store instances on all edge points.
-  (It's an eventually consistent KV storage.)
+  KV is eventually-consistent, so:
+
+  "Changes are usually immediately visible in the Cloudflare
+  global network location at which they are made but may
+  take up to 60 seconds or more to be visible in other
+  global network locations as their cached versions
+  of the data time out."
+
+  src:
+  https://developers.cloudflare.com/workers/learning/how-kv-works/#consistency
   */
   const maxKVsyncTime = 60; // 60 sec = 1 min
+
+  const expiryTimestamp = useMemo(() => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + maxKVsyncTime);
+    return time;
+  }, []);
+
+  const { seconds, isRunning, restart, start } = useTimer({
+    expiryTimestamp,
+    autoStart: false,
+    onExpire: () => console.warn("onExpire called"),
+  });
 
   const updateObjectAccess = api.example.updateObjectAccess.useMutation({
     onSuccess: async () => {
       await utils.example.getAllImages.invalidate();
+      const time = new Date();
+      time.setSeconds(time.getSeconds() + maxKVsyncTime);
+      restart(time);
     },
     onError: () => {
       // revert to the old value
@@ -46,26 +77,51 @@ function MakeImagePublicSwitch({ imageData }: ImageDataProps) {
     });
   }
 
+  const progressValue = useMemo(() => {
+    return 100 - Math.floor((seconds / maxKVsyncTime) * 100);
+  }, [seconds]);
+
   return (
-    <Switch
-      isDisabled={updateObjectAccess.isLoading}
-      size="sm"
-      isSelected={isPublic}
-      onValueChange={(v) => void changeObjectAccess(v)}
-      startContent={<EyeIcon />}
-      endContent={<EyeOffIcon />}
-      className="group/switch mt-2"
-    >
-      <span className="opacity-50 transition-opacity group-data-[selected=true]/switch:opacity-100">
-        Make image public
-      </span>
-    </Switch>
+    <>
+      <Switch
+        isDisabled={updateObjectAccess.isLoading}
+        size="sm"
+        isSelected={isPublic}
+        onValueChange={(v) => void changeObjectAccess(v)}
+        startContent={<EyeIcon />}
+        endContent={<EyeOffIcon />}
+        className="group/switch mt-2"
+      >
+        <span className="opacity-50 transition-opacity group-data-[selected=true]/switch:opacity-100">
+          Make image public
+        </span>
+      </Switch>
+      <AnimatePresence>
+        {seconds !== 0 && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 100 }}
+            exit={{ opacity: 100 }}
+          >
+            <Progress
+              aria-label="SynchronizationProgress"
+              size="sm"
+              value={progressValue}
+              label="Synchronizing changes..."
+              color="success"
+              showValueLabel={true}
+              className="mt-2"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
 
 function ImageCard({ imageData }: ImageDataProps) {
   return (
-    <Card className="h-56 cursor-pointer py-4">
+    <Card className="h-60 cursor-pointer py-4">
       <CardBody className="flex  flex-row gap-8 py-2">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <div className="w-1/2">
@@ -84,7 +140,7 @@ function ImageCard({ imageData }: ImageDataProps) {
             </div>
           )}
         </div>
-        <div className="flex h-full w-1/2 flex-col">
+        <div className="min-w-1/2 flex h-full w-1/2 flex-col">
           <h2 className="mb-2 font-semibold">{imageData.filename}</h2>
           <small className="block text-neutral-400">
             {prettyBytes(imageData.size)}
